d394514da23685ecac6dcb8b73242c2e0969e236
kuldeep7688 <7688kuldeepsingh@gmail.com>
2022-10-25 19:28:30 -0400

    added graphs for airport analysis and phase analysis

--- application_code.py
+++ application_code.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import altair as alt
 import pandas as pd
-
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+from utilities import preprocess_text_fields, remapping_function
+from collections import Counter
+st.set_page_config(layout="wide")
 
 # Title and subtitle
 st.write("""
@@ -32,6 +37,16 @@ The dataset can be found at [FAA Wildlife strikes Data](https://wildlife.faa.gov
 # ADD SEPARATOR
 
 # some required preprocessing
+df = preprocess_text_fields(df)
+df = remapping_function(df)
+columns_to_remove = [
+    'NR_FATALITIES', 'NR_INJURIES', 'EFFECT_OTHER',  'LOCATION', 
+    'ENG_1_POS', 'ENG_2_POS', 'ENG_3_POS', 'ENG_4_POS', 'LATITUDE', 'LONGITUDE', 
+    # 'STR_RAD', 'DAM_RAD', 'STR_WINDSHLD', 'DAM_WINDSHLD', 'STR_NOSE', 'DAM_NOSE', 'STR_ENG1', 'DAM_ENG1', 'STR_ENG2', 
+    # 'DAM_ENG2', 'STR_ENG3', 'DAM_ENG3', 'STR_ENG4', 'DAM_ENG4', 'STR_PROP', 'DAM_PROP', 'STR_WING_ROT', 'DAM_WING_ROT', 
+    # 'STR_FUSE', 'DAM_FUSE', 'STR_LG', 'DAM_LG', 'STR_TAIL', 'DAM_TAIL', 'STR_LGHTS', 'DAM_LGHTS', 'STR_OTHER', 'DAM_OTHER',
+]
+df = df[[c for c in list(df.columns) if c not in columns_to_remove]]
 df['INCIDENT_YEAR'] = pd.to_datetime(df["INCIDENT_YEAR"], format='%Y')
 
 # showing of data and graphs and df.head
@@ -41,11 +56,12 @@ st.write(
     ##### Introduction Graphs and Dataframe
     '''
 )
-int_tab_1, int_tab_2, int_tab_3, int_tab_4 = st.tabs(
+int_tab_1, int_tab_2, int_tab_3, int_tab_4, int_tab_5 = st.tabs(
     [
         'Total Strikes', 'Strikes with Small Birds',
         'Strikes with Medium or Large Birds',
-        'Dataframe'
+        'Dataframe',
+        'Initial Preprocessing Steps'
     ]
 )
 with int_tab_1:
@@ -96,8 +112,39 @@ with int_tab_4:
     temp_df = df.sample(num_rows_to_see)
     st.dataframe(temp_df)
 
+with int_tab_5:
+    st.header('Initial Preprocessing Steps :')
+    st.write(
+        '''
+        1) Preprocessing text fields and converting empty strings to None values after going through the
+            field information.
+        2) Remapping classes of ordinal and nominal variables.
+        3) Converting fields to a particular datatype instead of object dataype.
+        4) Normalizing null values all across the dataframe as 'None' has a different meaning in some of the columns.
+        '''
+    )
+
 # show the missing value heatmap 
+st.write(
+    '''
+    #### HeatMap for assessing missingness type.
+    Info : Since data is too big showing map on randomly sampled 50k subset.
+    '''
+)
+# m_df = df.sample(50000)
+# missing_value_heatmap = plt.figure(figsize=(20,20))
+# sns.heatmap(m_df.isna().transpose(), cmap="viridis")
+# st.pyplot(missing_value_heatmap)
 # and mention the missing type 
+st.write(
+    '''
+    #### Observations :
+    1) Cost related fields have a lot of Null values
+    2) NUM_SEEN, WARNED, EFFECT also have fairly high number of Null values.
+    3) Missingness type is Missingness at Random, as most of the values can be inferred from other fields using rules.
+    - Some suggestions are provided to fill the missing values.
+    '''
+)
 
 # subsetting the data by taking values from the user
 st.write(
@@ -134,6 +181,146 @@ options_time_of_day = st.multiselect(
 df = df[df['TIME_OF_DAY'].isin(options_time_of_day)]
 print('dataframe shape after subsetting for time of day is : {}'.format(df.shape))
 
-# add graph of airports and states with  most strikes  (Side by Side)
-# add graph of airports and states with most damage 
+# Dive into Airport Analysis
+st.write(
+    '''
+    #### Airport Analysis of 100 Airports with most Strikes
+    '''
+)
+# air_tab_1, air_tab_2, air_tab_3, air_tab_4 = st.tabs(
+#     [
+#         'Number of Strikes during the period selected:', 
+#         'Number of Strikes with distinction in Damage Level',
+#         'Number of Strikes with Minor or Substantial Damage',
+#         'Damage Level Trends'
+#     ]
+# )
+
+# # selecting 100 most count airports
+# hundred_most_ws_airports = [
+#     i[0] for i in Counter(df[df.AIRPORT != 'UNKNOWN'].AIRPORT.to_list()).most_common(100)
+# ]
+# with air_tab_1:
+#     st.write('#### Number of Strikes at top 100 Airports')
+#     chart_5 = alt.Chart(df[df.AIRPORT.isin(hundred_most_ws_airports)]).mark_bar(size=10).encode(
+#         x='AIRPORT:N',
+#         y='count()',
+#         # column='DAMAGE_LEVEL:N',
+#     ).properties(
+#         width=1500,
+#         height=500
+#     )
+#     st.altair_chart(chart_5, use_container_width=True)
+
+
+# with air_tab_2:
+#     st.write('#### Number of Strikes at top 100 Airports with Damage Level')
+#     chart_6 = alt.Chart(df[df.AIRPORT.isin(hundred_most_ws_airports)]).mark_bar(size=10).encode(
+#         x='AIRPORT:N',
+#         y='count()',
+#         color='DAMAGE_LEVEL:N',
+#     ).properties(
+#         width=1500,
+#         height=500
+#     )
+#     st.altair_chart(chart_6, use_container_width=True)
+
+
+# with air_tab_3:
+#     temp_df = df[df.DAMAGE_LEVEL.isin(['Substantial', 'Destroyed'])]
+#     hundred_most_ws_airports = [
+#         i[0] for i in Counter(temp_df[temp_df.AIRPORT != 'UNKNOWN'].AIRPORT.to_list()).most_common(100)
+#     ]
+#     st.write('#### Number of Strikes at top 100 Airports with Damage Level Substantial or Destroyed')
+#     chart_7 = alt.Chart(temp_df[temp_df.AIRPORT.isin(hundred_most_ws_airports)]).mark_bar(size=10).encode(
+#         x='AIRPORT:N',
+#         y='count()',
+#         color='DAMAGE_LEVEL:N',
+#     ).properties(
+#         width=1500,
+#         height=500
+#     )
+#     st.altair_chart(chart_7, use_container_width=True)
+
+# with air_tab_4:
+#     temp_df = df[['INCIDENT_YEAR', 'DAMAGE_LEVEL']].groupby('INCIDENT_YEAR').agg(
+#         no_damage=('DAMAGE_LEVEL', lambda x: Counter(x)['No_Damage']),
+#         minor_damage=('DAMAGE_LEVEL', lambda x: Counter(x)['Minor']),
+#         substantial_damage=('DAMAGE_LEVEL', lambda x: Counter(x)['Substantial']),
+#         destroyed=('DAMAGE_LEVEL', lambda x: Counter(x)['Destroyed'])
+#     ).reset_index()
+#     temp_df = temp_df.melt(id_vars=['INCIDENT_YEAR'], value_vars=['minor_damage', 'substantial_damage', 'destroyed'])
+#     chart_8 = alt.Chart(temp_df).mark_line().encode(
+#         alt.X('INCIDENT_YEAR', axis=alt.Axis(format='%Y')),
+#         y='value:Q',
+#         color='variable:N'
+#     )
+#     st.altair_chart(chart_8, use_container_width=True)
+
+st.write(
+    '''
+    #### Observations :
+    1)
+    2) 
+    3)
+    '''
+) 
+
+
+st.write(
+    '''
+    ### Phase of Flight Analysis
+    '''
+)
+# phase of flight with with number of birds and damage level analysis
+ph_tab_1, ph_tab_2, ph_tab_3 = st.tabs(
+    [
+        'PHASE_OF_FLIGHT Counts', 
+        'Phase of Flight with Number of birds struck',
+        'Phase of flight with number of birds struct and damage level',
+    ]
+)
+
+with ph_tab_1:
+    st.write('#### Phase of Flight')
+    chart_9 = alt.Chart(df).mark_bar().encode(
+        x='PHASE_OF_FLIGHT:N',
+        y='count()',
+    ).properties(
+        width=1500,
+        height=500
+    ).interactive()
+    st.altair_chart(chart_9, use_container_width=True)
+
+with ph_tab_2:
+    st.write('#### Phase of Flight with Number of birds struck')
+    chart_10 = alt.Chart(df).mark_bar().encode(
+        x='PHASE_OF_FLIGHT:N',
+        y='count()',
+        color='NUM_STRUCK'
+    ).properties(
+        width=1500,
+        height=500
+    ).interactive()
+    st.altair_chart(chart_10, use_container_width=True)
+
+
+with ph_tab_3:
+    st.write('#### Phase of Flight with Num of Birds and Damage Level')
+    chart_11 = alt.Chart(df).mark_bar().encode(
+        x='PHASE_OF_FLIGHT:N',
+        y='count()',
+        color='NUM_STRUCK',
+        column='DAMAGE_LEVEL'
+    ).interactive()
+    st.altair_chart(chart_11)
+
+st.write(
+    '''
+    #### Observations :
+    1)
+    2) 
+    3)
+    '''
+) 
 # add graph of airports and states with most cost and compare with adjusted cost
---
+++ utilities.py
@@ -0,0 +1,156 @@
+import numpy as np
+import pandas as pd
+
+
+# column types
+# types of columns
+CONTINUOUS_COLUMNS = [ # has both float and int columns
+    'INDX_NR', 'LATITUDE', 'LONGITUDE', 
+    'NUM_ENGS', 'ENG_1_POS', 'ENG_2_POS', 'ENG_3_POS', 'ENG_4_POS', # are ordered columns too
+    'HEIGHT', 'SPEED', 'DISTANCE', 'AOS', 'COST_REPAIRS', 'COST_OTHER', 'COST_REPAIRS_INFL_ADJ', 'COST_OTHER_INFL_ADJ',
+    'NR_INJURIES', 'NR_FATALITIES', 
+    
+]
+
+NOMINAL_COLUMNS = [
+    'TIME_OF_DAY', 'STATE', 'AIRPORT', 'AIRPORT_ID', 'FAAREGION', 'OPID', 'OPERATOR', 'REG', 'AIRCRAFT', 'EMA', 'EMO',
+    'AMA', 'AMO', 'AC_CLASS', 'AC_MASS', 'TYPE_ENG', 'PHASE_OF_FLIGHT', 'SKY', 'PRECIPITATION', 'SPECIES_ID', 'SPECIES',
+    
+]
+
+ORDINAL_COLUMNS = [
+    'DAMAGE_LEVEL', 'NUM_SEEN', 'NUM_STRUCK', 'SIZE'
+]
+
+TEXT_COLUMNS = [
+    'LOCATION', 
+    'FLT', # specific to airlines can be explored further.,
+    'EFFECT', 'EFFECT_OTHER', 
+]
+
+BOOLEAN_COLUMNS = [
+    'INDICATED_DAMAGE', 'STR_RAD', 'DAM_RAD', 'STR_WINDSHLD', 'DAM_WINDSHLD', 'STR_NOSE', 'DAM_NOSE', 'STR_ENG1', 'DAM_ENG1', 
+    'STR_ENG2', 'DAM_ENG2', 'STR_ENG3', 'DAM_ENG3', 'STR_ENG4', 'DAM_ENG4', 'STR_PROP', 'DAM_PROP', 'STR_WING_ROT', 'DAM_WING_ROT', 
+    'STR_FUSE', 'DAM_FUSE', 'STR_LG', 'DAM_LG', 'STR_TAIL', 'DAM_TAIL', 'STR_LGHTS', 'DAM_LGHTS', 'STR_OTHER', 'DAM_OTHER',
+    'WARNED',
+]
+
+DATETIME_COLUMNS = [
+    'INCIDENT_DATE ', 'INCIDENT_MONTH', 'INCIDENT_YEAR', 'TIME',
+]
+
+
+# mapping dictionaries
+PRECIPITATION_MAPPING = {
+    'Rain': 'Rain',
+    'Fog': 'Fog',
+    'Snow': 'Snow',
+    'None': 'No_precipitation',
+    'Fog, Rain': 'Fog_plus_Rain',
+    'Rain, Snow': 'Rain_plus_Snow',
+    'Fog, Rain, Snow': 'Fog_plus_Rain_plus_Snow',
+    'Fog, Snow': 'Fog_plus_Snow',
+    'Snow, None': 'Snow',
+    'Fog, None': 'Fog',
+    'Rain, None': 'Rain',
+    'Rain, Snow, None': 'Rain_plus_Snow'
+}
+
+DAMAGE_LEVEL_MAPPING = {
+    'N': 'No_Damage',
+    'S': 'Substantial',
+    'M': 'Minor',
+    'M?': 'Minor',
+    'D': 'Destroyed'
+}
+
+EFFECT_MAPPING = {
+    'None': 'No_Effect',
+    'None, Aborted Take-off': 'Aborted Take-off', 
+    'None, Precautionary Landing': 'Precautionary Landing', 
+    'Precautionary Landing': 'Precautionary Landing', 
+    'Other': 'Precautionary Landing', 
+    'None, Other': 'Other', 
+    'Aborted Take-off': 'Aborted Take-off', 
+    'None, Engine Shutdown': 'Engine Shutdown', 
+    'Engine Shutdown': 'Engine Shutdown', 
+    'Precautionary Landing, Engine Shutdown': 'Precautionary Landing with Engine Shutdown', 
+    'Precautionary Landing, Other': 'Precautionary Landing', 
+    'None, Precautionary Landing, Engine Shutdown': 'Precautionary Landing with Engine Shutdown',
+    'None, Precautionary Landing, Other': 'Precautionary Landing', 
+    'None, Aborted Take-off, Other': 'Aborted Take-off', 
+    'None, Aborted Take-off, Engine Shutdown': 'Aborted Take-off with Engine Shutdown', 
+    'None, Aborted Take-off, Engine Shutdown, Other': 'Aborted Take-off with Engine Shutdown', 
+    'None, Precautionary Landing, Engine Shutdown, Other': 'Precautionary Landing with Engine Shutdown', 
+    'Aborted Take-off, Engine Shutdown': 'Aborted Take-off with Engine Shutdown'
+}
+
+WARNED_MAPPING = {
+    'Yes': 'Yes',
+    'No': 'No',
+    'Unknown': np.nan,
+}
+
+AC_MASS_MAPPING = {
+    '1.0': '<= 2250 kg', 
+    '2.0': '2251 to 5700 kg',
+    '3.0': '5701-27000 kg', 
+    '4.0': '27001-272000 kg', 
+    '5.0': '> 272000 kg', 
+}
+
+TYPE_ENGINE_MAPPING = {
+    'D': 'Turbofan', 
+    'C': 'Turboprop',
+    'F': 'Turboshaft (helicopter)',
+    'A': 'Reciprocating Engine', 
+    'B': 'Turbojet', 
+    'Y': 'Other', 
+    'E': 'No_Engine'
+}
+
+
+# for all nominal, ordinal, text and INCIDENT_DATE and TIME strip the strings and make empty string as None
+def text_preprocess_helper_func(x):
+    if x == x:
+        if x == str:
+            return x.strip()
+        else:
+            return str(x).strip()
+    else:
+        return x
+
+
+def preprocess_text_fields(df):
+    # print('''
+    # \n\n\n
+    # Preprocessing happening.
+    # \n\n\n
+    # ''')
+    columns_to_be_stripped = NOMINAL_COLUMNS + TEXT_COLUMNS + ['INCIDENT_DATE', 'TIME', 'NUM_STRUCK', 'NUM_SEEN']
+    for col in columns_to_be_stripped:
+        # print(col)
+        df[col] = df[col].apply(text_preprocess_helper_func)
+    
+    # now convertinf empty strings to None values
+    for col in columns_to_be_stripped:
+        df.loc[df[col] == '', col] = np.nan
+    
+    return df
+
+
+# remapping precipitation
+def map_to_provided_labels(x, mapping):
+    if x == x:
+        return mapping[x]
+    else:
+        return x
+
+def remapping_function(df):
+    df['PRECIPITATION'] = df.PRECIPITATION.apply(lambda x : map_to_provided_labels(x, PRECIPITATION_MAPPING))
+    df['DAMAGE_LEVEL'] = df.DAMAGE_LEVEL.apply(lambda x : map_to_provided_labels(x, DAMAGE_LEVEL_MAPPING))
+    df['EFFECT'] = df.EFFECT.apply(lambda x : map_to_provided_labels(x, EFFECT_MAPPING))
+    df['WARNED'] = df.WARNED.apply(lambda x : map_to_provided_labels(x, WARNED_MAPPING))
+    df['AC_MASS'] = df.AC_MASS.apply(lambda x : map_to_provided_labels(x, AC_MASS_MAPPING))
+    df['TYPE_ENG'] = df.TYPE_ENG.apply(lambda x : map_to_provided_labels(x, TYPE_ENGINE_MAPPING))
+    return df
